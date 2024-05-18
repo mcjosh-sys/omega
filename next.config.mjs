40de23189f24678b7d0e1b216f690f1ca7aa73f7
Joshua <olusholajoshua7@gmail.com>
2024-05-18 10:16:25 -0600

    fixed signin and signup

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,43 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'uploadthing.com'
-            },
-            {
-                protocol: 'https',
-                hostname: 'utfs.io'
-            },
-            {
-                protocol: 'https',
-                hostname: 'img.clerk.com'
-            },
-            {
-                hostname: 'subdomain'
-            },
-            {
-                protocol: 'https',
-                hostname: 'files.stripe.com'
-            },
-        ]
-    },
-    reactStrictMode: false
-};
+  images: {
+    domains: [
+      'uploadthing.com',
+      'utfs.io',
+      'img.clerk.com',
+      'subdomain',
+      'files.stripe.com',
+    ],
+  },
+  reactStrictMode: false,
+}
+
+// const nextConfig = {
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: 'https',
+//                 hostname: 'uploadthing.com'
+//             },
+//             {
+//                 protocol: 'https',
+//                 hostname: 'utfs.io'
+//             },
+//             {
+//                 protocol: 'https',
+//                 hostname: 'img.clerk.com'
+//             },
+//             {
+//                 hostname: 'subdomain'
+//             },
+//             {
+//                 protocol: 'https',
+//                 hostname: 'files.stripe.com'
+//             },
+//         ]
+//     },
+//     reactStrictMode: false
+// };
 
 export default nextConfig;
